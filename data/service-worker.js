e5dc8b2a6b36862bb9ae05f23b2ce5cfe8cb7825
tinokia <ti902188@gmail.com>
2026-08-01 22:35:12 +0700

    Add files via upload

---
+++ data/service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "kpc-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "doc.html",
+  "data/style.css",
+  "data/script.js",
+  "data/kinhphapcu.js",
+  "data/clock.js",
+  "data/HH192.png",
+  "data/HH512.png"
+];
+
+// Cài đặt
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+// Lấy dữ liệu offline
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
